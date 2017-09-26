a11eaadf8408cf23c2db20a02229024f10242eaf
Abby Christens <achristens@gmail.com>
2017-09-26 18:09:08 -0400

    Adding exercise 3

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,37 @@
-# Exercise 1:
-# Create an emotions hash, where the keys are the names of
-# different human emotions and the values are the degree to
-# which the emotion is being felt on a scale from 1 to 3.
-
-emotions {
-  :happiness => (1..3),
-  :stress => (1..3),
-  :fear => (1..3)
+emotions = {
+  :happiness => 3,
+  :stress => 2,
+  :fear => 1
 }
+
+class Person
+  def initialize(name, emotions)
+    @name = name
+    @emotions = emotions
+  end
+
+  def emotions
+    @emotions
+  end
+
+  def name
+    @name
+  end
+
+  def message
+    emotions.each do | k,v |
+      if v == 1
+        puts "I am feeling a low amount of #{k}."
+      elsif v == 2
+        puts "I am feeling a medium amount of #{k}."
+      else
+        puts "I am feeling a high amount of #{k}."
+      end
+    end
+  end
+
+end
+
+abby = Person.new("Abby", emotions)
+# puts abby.inspect
+puts abby.message
