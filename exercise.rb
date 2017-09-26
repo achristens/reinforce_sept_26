9e5984f866dc0096a6a077fcc718809b199da075
Abby Christens <achristens@gmail.com>
2017-09-26 16:31:55 -0400

    Exercise one, adding hash

---
+++ exercise.rb
@@ -0,0 +1,10 @@
+# Exercise 1:
+# Create an emotions hash, where the keys are the names of
+# different human emotions and the values are the degree to
+# which the emotion is being felt on a scale from 1 to 3.
+
+emotions {
+  :happiness => (1..3),
+  :stress => (1..3),
+  :fear => (1..3)
+}
